39844a32f759020cdd4d9332726548b7b0d6bd5c
an <805595674@qq.com>
2023-03-20 16:31:57 +0800

    第四次提交

--- app/src/main/java/com/acg/gitstudy/MainActivity.kt
+++ app/src/main/java/com/acg/gitstudy/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this,"这里是main分支！！",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,"这是main分支的第四次提交！！",Toast.LENGTH_SHORT).show()
     }
 }
